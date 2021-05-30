af9368892e221bad74b076f826ca2de80c3fa220
joeikedo <54558160+joeikedo@users.noreply.github.com>
2021-05-30 10:12:50 -0700

    log button

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
-console.log('hi');
+//Console log demo button
+document.getElementById("log-button").addEventListener("click", function() {
+    console.log(document.getElementById('log-button').innerText);
+  });
